875ebfa47005b8d7f04beebf6fdabab0356ce0c2
tricia gray <tgray@tricias-MacBook-Pro.local>
2023-04-12 00:29:02 -0400

    updated

--- fragments/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import AllFragments from './src/components/AllFragments'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './'
-})

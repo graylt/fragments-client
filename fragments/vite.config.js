d05f5b011bf3b00ed7f941b5e5726d745a51bcf0
tricia gray <tgray@tricias-MacBook-Pro.local>
2023-04-11 23:36:23 -0400

    deploying to pages

--- fragments/vite.config.js
+++ fragments/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AllFragments from './src/components/AllFragments'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fragments'
 })
